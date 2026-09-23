0ece866b9adee922d5f75376eb53749576ed16d6
Gabriel Ricardo Ramirez Tellez <ramireztellezgr@gmail.com>
2026-09-22 22:23:24 -0500

    minusculas

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -275,7 +275,7 @@ return palabras.length;
     public String convertirAMinusculas(String cadena) {
         // TODO: Implementar el método para convertir una cadena a minúsculas.
         // Ejemplo: Si cadena = "HELLO", el resultado debería ser "hello".
-        return "";
+        return caden.toLowerCase();
     }
 
     // Método que reemplaza una subcadena en una cadena por otra subcadena
