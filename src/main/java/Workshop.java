2c39ca7daab24acafd666790a63b976c98c95b9d
Gabriel Ricardo Ramirez Tellez <ramireztellezgr@gmail.com>
2026-09-22 21:00:35 -0500

    combinar arreglos

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -201,9 +201,15 @@ return resultado;
     public int[] combinarArreglos(int[] arreglo1, int[] arreglo2) {
         // TODO: Implementar el método para combinar dos arreglos en uno solo.
         // Ejemplo: Si arreglo1 = [1, 2, 3, 4, 5] y arreglo2 = [6, 7, 8], el resultado debería ser [1, 2, 3, 4, 5, 6, 7, 8].
-        return new int[0];
-    }
-
+int[] combinado = new int[arreglo1.length + arreglo2.length];
+for (int i = 0; i < arreglo1.length; i++) {
+combinado[i] = arreglo1[i];
+}
+for (int i = 0; i < arreglo2.length; i++) {
+combinado[arreglo1.length + i] = arreglo2[i];
+}
+return combinado;
+}
     // Método que rota un arreglo n posiciones
     public int[] rotarArreglo(int[] arreglo, int posiciones) {
         // TODO: Implementar el método para rotar un arreglo n posiciones.
