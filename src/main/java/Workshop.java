63880131adf3e26273b42ccd71e1ded1c60aa7b6
Gabriel Ricardo Ramirez Tellez <ramireztellezgr@gmail.com>
2026-09-22 19:12:00 -0500

    promedio arreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -99,7 +99,12 @@ suma += arreglo[i];
     public double promedioElementos(int[] arreglo) {
         // TODO: Implementar el método para calcular el promedio de los elementos de un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 3.0.
-        return 0.0;
+int suma = 0;
+for(int i=0; i<arreglo.length; i++){
+suma += arreglo[i];
+}
+double prom = suma / arreglo.length;
+        return prom;
     }
 
     // Método que encuentra el elemento mayor en un arreglo
